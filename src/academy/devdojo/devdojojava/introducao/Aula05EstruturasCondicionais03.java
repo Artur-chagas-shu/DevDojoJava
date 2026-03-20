ae4ac5829473f0e10675762283b861a80c7a7c4c
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-20 16:27:29 -0300

    operador ternario

---
+++ src/academy/devdojo/devdojojava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.devdojojava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    static void main(String[] args) {
+        double salario = 4000;
+        //(Condicao) ? verdadeiro : falso
+
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições, mas vou ter!" ;
+
+        System.out.println(resultado);
+
+
+    }
+}
