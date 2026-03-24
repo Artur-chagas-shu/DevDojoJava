0acf76f663068c70ef94e91a2e782aaa3a74d18d
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-24 16:27:12 -0300

    Introducao a oriantacao a objetos

---
+++ src/academy/devdojo/devdojojava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.devdojojava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/devdojojava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.devdojojava.javacore.introducaoclasses.test;
+
+import academy.devdojo.devdojojava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
+
