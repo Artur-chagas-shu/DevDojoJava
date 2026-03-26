cef795f501b0576668f4d9e751c37c221b50260f
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-25 23:19:49 -0300

    Parametros

---
+++ src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.devdojojava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.devdojojava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
