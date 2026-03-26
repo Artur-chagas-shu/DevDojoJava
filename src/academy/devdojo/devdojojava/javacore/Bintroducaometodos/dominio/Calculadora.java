f45e57bdd84c505a365be43081e423233e606163
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-25 23:28:58 -0300

    Retorno

--- src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -9,7 +9,13 @@ public class Calculadora {
         System.out.println(21 - 2);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2) {
+    public void multiplicaDoisNumeros(int num1, float num2) {
         System.out.println(num1 * num2);
+    }
+    public double divideDoisNumeros (double num1, double num2){
+        if(num2 == 0){
+            return 0 ;
+        }
+        return  num1 / num2;
     }
 }
---
+++ src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.devdojojava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.devdojojava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+       double resultado = calculadora.divideDoisNumeros(20,2);
+        System.out.println(resultado);
+
+    }
+}
