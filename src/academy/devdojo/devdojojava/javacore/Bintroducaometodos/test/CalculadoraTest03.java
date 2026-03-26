2b196f0817aff63367acdd673e42041f0c819c67
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-25 23:41:37 -0300

    retorno final

--- src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -7,6 +7,9 @@ public class CalculadoraTest03 {
         Calculadora calculadora = new Calculadora();
        double resultado = calculadora.divideDoisNumeros(20,2);
         System.out.println(resultado);
+        System.out.println(calculadora.divideDoisNumeros02(20,0));
+        System.out.println("-----------------");
+        calculadora.imprimeDivisaoDeDoisNumeros02(86,0);
 
     }
 }
