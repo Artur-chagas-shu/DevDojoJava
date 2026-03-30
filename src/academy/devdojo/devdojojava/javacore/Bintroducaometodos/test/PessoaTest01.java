0f94f8f740a52ee8b7ea0e410df70d11a4f0cb5b
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-30 00:43:19 -0300

    modificador de acesso privado, coesao e acoplamento

---
+++ src/academy/devdojo/devdojojava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.devdojojava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.devdojojava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+        //pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
