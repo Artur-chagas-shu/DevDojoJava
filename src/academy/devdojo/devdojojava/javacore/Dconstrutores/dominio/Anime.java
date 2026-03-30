7a8230d1b003ad6e7556bcdb9c891f2afca73c64
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-30 17:04:41 -0300

    Construtores

--- src/academy/devdojo/devdojojava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdojo/devdojojava/javacore/Dconstrutores/dominio/Anime.java
@@ -1,4 +1,4 @@
-package academy.devdojo.devdojojava.javacore.Csobrecargametodos.dominio;
+package academy.devdojo.devdojojava.javacore.Dconstrutores.dominio;
 
 public class Anime {
     private String nome;
@@ -6,22 +6,18 @@ public class Anime {
     private int episodios;
     private String genero;
 
-    public Anime(){
-
-    }
-
-    public void init(String nome, String tipo, int episodios) {
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        System.out.println("Dentro do construtor");
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
-
-
-    }
-
-    public void init(String nome, String tipo, int episodios, String genero) {
-       this.init(nome,tipo,episodios);
         this.genero = genero;
+
     }
+
+    public Anime() {
+    }
+
 
     public void imprime() {
         System.out.println(this.nome);
