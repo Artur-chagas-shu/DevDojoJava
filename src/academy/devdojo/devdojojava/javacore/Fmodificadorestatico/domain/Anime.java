f3b237becaa71b8c978ac1617889ec010bc0dea5
Artur Chagas <artur_life_2015@hotmail.com>
2026-03-31 15:19:39 -0300

    Bloco de inicializacao estatico

---
+++ src/academy/devdojo/devdojojava/javacore/Fmodificadorestatico/domain/Anime.java
@@ -0,0 +1,52 @@
+package academy.devdojo.devdojojava.javacore.Fmodificadorestatico.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    //0 - bloco de inicialização é executado quando a JVM carregar a classe
+    // 1 - Alocado espaco em memoria pro objeto
+    // 2- Cada atributo de classe é criado e inicializado com valores default ou o que for passado
+    // 3- Bloco de inicializacao é executado
+    // 4- Construtor é executado
+    static {
+        System.out.println("Dentro do bloco de inicializacao");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao 1");
+    }
+    static {
+        System.out.println("Dentro do bloco de inicializacao 2");
+    }
+    {
+        System.out.println("bloco de atualizacao nao estatico");
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
+
+
+
+
---
+++ src/academy/devdojo/devdojojava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.devdojojava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.devdojojava.javacore.Fmodificadorestatico.domain.Anime;
+
+public class AnimeTest01 {
+    static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+    }
+}
