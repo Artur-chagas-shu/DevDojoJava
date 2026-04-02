ccdd22518d608042804e576aba8ba38157074934
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-02 17:38:56 -0300

    protected

--- src/academy/devdojo/devdojojava/javacore/Hheranca/test/Funcionario2.java
+++
@@ -1,9 +0,0 @@
-package academy.devdojo.devdojojava.javacore.Hheranca.test;
-
-import academy.devdojo.devdojojava.javacore.Hheranca.dominio.Pessoa;
-
-public class Funcionario2 extends Pessoa {
-    public void imprime(){
-        this.nome = "Sasa";
-    }
-}
