ccb72f64b393640b290864382b94778e4afae122
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-02 00:56:38 -0300

    Sobrescrita , Super

--- src/academy/devdojo/devdojojava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/devdojojava/javacore/Hheranca/dominio/Funcionario.java
@@ -3,7 +3,10 @@ package academy.devdojo.devdojojava.javacore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
-
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
     public double getSalario() {
         return salario;
     }
