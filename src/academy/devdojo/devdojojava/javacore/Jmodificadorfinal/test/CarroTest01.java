0e100bcee4d74c232a1171d93cdf7548443693ba
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-03 01:46:12 -0300

    Constant Final

---
+++ src/academy/devdojo/devdojojava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.devdojojava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.devdojojava.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+    static void main() {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
