d188d64ee8746b10465a773f42baf79eb4c313d9
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-03 01:56:28 -0300

    Constant Final

--- src/academy/devdojo/devdojojava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdojo/devdojojava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,10 +1,14 @@
 package academy.devdojo.devdojojava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.devdojojava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.devdojojava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest01 {
     static void main() {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
     }
 }
