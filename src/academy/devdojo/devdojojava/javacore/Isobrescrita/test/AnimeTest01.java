6b001a47f1581a79352e775f95cb87130bf7e59e
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-02 23:33:28 -0300

    toString

---
+++ src/academy/devdojo/devdojojava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.devdojojava.javacore.Isobrescrita.test;
+
+import academy.devdojo.devdojojava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    static void main() {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+
+    }
+}
