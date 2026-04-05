a52fbc0a48994e14abd3d97aa8bf88a6748d98bc
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-05 01:03:58 -0300

    ENUMERACAO,Busca por atributos

--- src/academy/devdojo/devdojojava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/devdojojava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,14 +1,25 @@
 package academy.devdojo.devdojojava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA(1,"Pessoa Fisica"),
-    PESSOA_JURIDICA(2,"Pessoa Juridica");
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
 
     private int valor;
     private String nomeRelatorio;
+
     TipoCliente(int valor, String nomeRelatorio) {
         this.valor = valor;
         this.nomeRelatorio = nomeRelatorio;
+    }
+
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio) {
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)) {
+                return tipoCliente;
+            }
+        }
+        return null;
     }
 
     public int getValor() {
--- src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
@@ -12,6 +12,10 @@ public class ClienteTest01 {
         System.out.println(cliente1);
         System.out.println(cliente2);
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Fisica22");
+        System.out.println(tipoCliente2);
 
     }
 }
