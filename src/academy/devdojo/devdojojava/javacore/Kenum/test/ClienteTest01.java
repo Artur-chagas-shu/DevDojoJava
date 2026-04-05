98e9230e76b86a327466b86ddc2155659d131ed0
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-05 00:48:23 -0300

    ENUMERACAO,Sobrescrita de métodos

--- src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
@@ -2,14 +2,16 @@ package academy.devdojo.devdojojava.javacore.Kenum.test;
 
 import academy.devdojo.devdojojava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.devdojojava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.devdojojava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     static void main() {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
 
     }
 }
