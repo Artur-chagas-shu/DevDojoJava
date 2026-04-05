18b90f9b2637269c985dbb076aa26b2975225e39
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-05 00:35:08 -0300

    ENUMERACAO, Construtores e atributos

--- src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/devdojojava/javacore/Kenum/test/ClienteTest01.java
@@ -5,13 +5,11 @@ import academy.devdojo.devdojojava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     static void main() {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+
     }
 }
