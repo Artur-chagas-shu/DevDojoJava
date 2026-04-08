300cfba8a7d1ed160108d51117bbe73b60638784
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-07 22:42:25 -0300

    Classes abstratas

---
+++ src/academy/devdojo/devdojojava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.devdojojava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.devdojojava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.devdojojava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    static void main(String[] args) {
+        Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 12000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+        gerente.imprime();
+        desenvolvedor.imprime();
+    }
+
+}
