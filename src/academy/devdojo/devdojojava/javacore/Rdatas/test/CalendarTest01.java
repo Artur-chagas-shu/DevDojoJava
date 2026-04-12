4f25f14aa3423ef18fa89b995c5d6a748a41e740
Artur Chagas <artur_life_2015@hotmail.com>
2026-04-12 17:58:54 -0300

    Calendar

--- src/academy/devdojo/devdojojava/javacore/Rdatas/test/CalendarTest01.java
+++ src/academy/devdojo/devdojojava/javacore/Rdatas/test/CalendarTest01.java
@@ -7,7 +7,7 @@ public class CalendarTest01 {
     static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
-            System.out.println("Domingao e o primeiro dia da semana");
+            System.out.println("Domingão  é o primeiro dia da semana");
         }
         System.out.println(c.get(Calendar.DAY_OF_WEEK));
         System.out.println(c.get(Calendar.DAY_OF_MONTH));
