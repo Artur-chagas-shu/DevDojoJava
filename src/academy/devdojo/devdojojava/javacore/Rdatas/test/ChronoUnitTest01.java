4574d19d2b34beb9452418aa7f2d09456db79eba
Artur Chagas <arturchagax@gmail.com>
2026-04-22 00:32:37 -0300

    classe ChronoUnit

---
+++ src/academy/devdojo/devdojojava/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.devdojojava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2000, Month.AUGUST, 29,15,0,0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println( ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println( ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println( ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println( ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
