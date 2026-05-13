c0b78b385f286b00a65251174da11e9eadada07a
Artur Chagas <arturchagax@gmail.com>
2026-05-12 23:17:13 -0300

    Generics pt 03 - Wildcard pt 02

--- src/academy/devdojo/devdojojava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/devdojojava/javacore/Zgenerics/test/WildcardTest02.java
@@ -1,23 +1,34 @@
 package academy.devdojo.devdojojava.javacore.Zgenerics.test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
     static void main() {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        List<Animal> animals = new ArrayList<Animal>();
+        printConsulta(animals);
+
 
 
     }
         //Type erasure
-    private static void printConsulta(List<Animal> animals){
+    private static void printConsulta(List<? extends Animal> animals){
         for (Animal animal : animals) {
            animal.consulta();
         }
 
-        animals.add(new Cachorro());
+    }
 
+    private static void printConsultaAnimal(List<? super Animal> animals) {
+           Animal animal = new Cachorro();
+           Animal animal2 = new Gato();
+           animals.add(new Cachorro());
+           animals.add(new Gato());
 
     }
 }
