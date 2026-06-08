84ed23333d71dc0890464247491c0892aecb011a
Artur Chagas <arturchagax@gmail.com>
2026-06-08 16:54:14 -0300

    Classes internas

---
+++ src/academy/devdojo/devdojojava/javacore/ZZAclassesinternas/test/OuterClassesTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.devdojojava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest01 {
+    private String name = "Monkey D. Luffy";
+
+
+    class Inner{
+        public void printOuterClassAttibute(){
+            System.out.println(name);
+            System.out.println(this);
+            System.out.println(OuterClassesTest01.this);
+        }
+    }
+
+
+    static void main() {
+        OuterClassesTest01 outerClass = new OuterClassesTest01();
+        Inner inner = outerClass.new Inner();
+        Inner inner2 = new OuterClassesTest01().new Inner();
+        inner.printOuterClassAttibute();
+        inner2.printOuterClassAttibute();
+
+
+    }
+}
