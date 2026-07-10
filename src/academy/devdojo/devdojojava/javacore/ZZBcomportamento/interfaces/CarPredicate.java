ddd6e4e233d216ba6cd5ad875f27934f2be5855b
Artur Chagas <arturchagax@gmail.com>
2026-07-10 15:23:32 -0300

    lambdas, Predicate, Consumer e Function

--- src/academy/devdojo/devdojojava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/devdojojava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,14 @@ package academy.devdojo.devdojojava.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.devdojojava.javacore.ZZBcomportamento.dominio.Car;
 
+
+@FunctionalInterface
 public interface CarPredicate {
+
+    //anonimas, funções , conciso
     boolean test (Car car);
+    //(parametro) -> <expressão>
+    //(Car car) -> car.getColor().equals("green")
+
+
 }
