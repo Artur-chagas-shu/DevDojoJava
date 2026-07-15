00f0b0f105df7028923d35726d6cc25d518ac28e
Artur Chagas <arturchagax@gmail.com>
2026-07-15 00:37:19 -0300

    Streams pt 04 - FlatMap pt 01

---
+++ src/academy/devdojo/devdojojava/javacore/ZZEstreams/teste/StreamTest04.java
@@ -0,0 +1,28 @@
+package academy.devdojo.devdojojava.javacore.ZZEstreams.teste;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StreamTest04 {
+    static void main(String[] args) {
+        List<List<String>> devdojo = new ArrayList<>();
+        List<String> graphicDesigner = List.of("Wildnei Suane", "Catarina Santos", "Sandy Carolina");
+        List<String> developers = List.of("William", "David", "Harisson");
+        List<String> students = List.of("Édipo", "Gustavo Lima", "Gustavo Mendes", "Guilherme");
+        devdojo.add(graphicDesigner);
+        devdojo.add(developers);
+        devdojo.add(students);
+
+        for (List<String> people : devdojo) {
+            for (String person : people) {
+                System.out.println(person);
+            }
+        }
+
+        System.out.println("-----------");
+        devdojo.stream()
+                .flatMap(Collection::stream).forEach(System.out::println);
+
+    }
+}
