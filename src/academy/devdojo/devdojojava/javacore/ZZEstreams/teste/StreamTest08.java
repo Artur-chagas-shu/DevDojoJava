1160ee01d1aac49a1265e18df6ed83032c8a3e45
Artur Chagas <arturchagax@gmail.com>
2026-07-15 17:01:26 -0300

    Streams pt 07 - Reduce pt 02

---
+++ src/academy/devdojo/devdojojava/javacore/ZZEstreams/teste/StreamTest08.java
@@ -0,0 +1,35 @@
+package academy.devdojo.devdojojava.javacore.ZZEstreams.teste;
+
+import academy.devdojo.devdojojava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamTest08 {
+    private static List<LightNovel> lightNovels = new ArrayList<>( List.of(
+            new LightNovel("Tensei shitara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game No Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    static void main(String[] args) {
+         lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum).ifPresent(System.out::println);
+
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+
+
+    }
+}
