4c306983756290433435684e11b866bf3c39dbde
Artur Chagas <arturchagax@gmail.com>
2026-07-16 16:57:30 -0300

    Streams pt 12 - Collectors pt 02 - Grouping by pt 01

---
+++ src/academy/devdojo/devdojojava/javacore/ZZEstreams/dominio/Category.java
@@ -0,0 +1,5 @@
+package academy.devdojo.devdojojava.javacore.ZZEstreams.dominio;
+
+public enum Category {
+    DRAMA, FANTASY, ROMANCE
+}
--- src/academy/devdojo/devdojojava/javacore/ZZEstreams/dominio/LightNovel.java
+++ src/academy/devdojo/devdojojava/javacore/ZZEstreams/dominio/LightNovel.java
@@ -5,13 +5,18 @@ import java.util.Objects;
 public class LightNovel {
     private String title;
     private double price;
+    private Category category;
 
     public LightNovel(String title, double price) {
         this.title = title;
         this.price = price;
     }
 
-
+    public LightNovel(String title, double price, Category category) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
+    }
 
     public String getTitle() {
         return title;
@@ -22,13 +27,16 @@ public class LightNovel {
 
     }
 
-
+    public Category getCategory() {
+        return category;
+    }
 
     @Override
     public String toString() {
         return "LightNovel{" +
                 "title='" + title + '\'' +
                 ", price=" + price +
+                ", category=" + category +
                 '}';
     }
 
